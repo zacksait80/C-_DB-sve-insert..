60462e8d3e48a7ddc1c746be3811ebc04561d4ee
Zack Said <zakisaeddahir@gmail.com>
2023-12-27 19:28:53 +0300

    Add project files.

---
+++ dbReader/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace dbReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPho = new System.Windows.Forms.TextBox();
+            this.txtNm = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.save = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(277, 211);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // txtId
+            // 
+            this.txtId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtId.Location = new System.Drawing.Point(404, 196);
+            this.txtId.Multiline = true;
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(420, 51);
+            this.txtId.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(35, 543);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 92;
+            this.dataGridView1.RowTemplate.Height = 37;
+            this.dataGridView1.Size = new System.Drawing.Size(1755, 505);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(894, 303);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 36);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PASWORD";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.Location = new System.Drawing.Point(1135, 289);
+            this.txtPass.Multiline = true;
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(420, 51);
+            this.txtPass.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(922, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 36);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "PHONE";
+            // 
+            // txtPho
+            // 
+            this.txtPho.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPho.Location = new System.Drawing.Point(1135, 196);
+            this.txtPho.Multiline = true;
+            this.txtPho.Name = "txtPho";
+            this.txtPho.Size = new System.Drawing.Size(420, 51);
+            this.txtPho.TabIndex = 9;
+            // 
+            // txtNm
+            // 
+            this.txtNm.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNm.Location = new System.Drawing.Point(404, 288);
+            this.txtNm.Multiline = true;
+            this.txtNm.Name = "txtNm";
+            this.txtNm.Size = new System.Drawing.Size(420, 51);
+            this.txtNm.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(273, 304);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 36);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "NAME";
+            // 
+            // save
+            // 
+            this.save.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.save.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.save.Location = new System.Drawing.Point(364, 426);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(195, 58);
+            this.save.TabIndex = 2;
+            this.save.Text = "save";
+            this.save.UseVisualStyleBackColor = false;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(658, 426);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 58);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "updata";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(986, 426);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(195, 58);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "delete";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button3.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Location = new System.Drawing.Point(1256, 53);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(168, 58);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "SEARCH";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(584, 67);
+            this.txtSearch.Multiline = true;
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(666, 44);
+            this.txtSearch.TabIndex = 15;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button4.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(1300, 426);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(195, 58);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "PRINT";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1814, 1055);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtNm);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPho);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPho;
+        private System.Windows.Forms.TextBox txtNm;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
+
---
+++ dbReader/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace dbReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection("Data Source=DESKTOP-T7U32P7;Initial Catalog=DBssave;Integrated Security=True");
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bind_data();
+        }
+        private void bind_data()
+        {
+            SqlCommand cmd = new SqlCommand("SELECT ID , NAME  ,PHONE, PASSWORD FROM dbsve", conn);
+            SqlDataAdapter sda  = new SqlDataAdapter();
+            sda.SelectCommand = cmd;
+            DataTable dt = new DataTable();
+            dt.Clear();
+            sda.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("tohome", 12, FontStyle.Bold);
+            dataGridView1.DefaultCellStyle.Font = new Font("arial",12,FontStyle.Bold);
+
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("insert into dbsve (ID,NAME , PHONE , PASSWORD)VALUES(@ID,@NAME  ,@PHONE, @PASSWORD )", conn);
+           if(txtId.Text == ""|| txtNm.Text == "" || txtPho.Text == "" || txtPass.Text == "")
+            {
+                MessageBox.Show("buuxi meesha banan..");
+            }
+           else
+            {
+                cmd.Parameters.AddWithValue("ID", txtId.Text);
+                cmd.Parameters.AddWithValue("NAME", txtNm.Text);
+                cmd.Parameters.AddWithValue("PHONE", txtPho.Text);
+                cmd.Parameters.AddWithValue("PASSWORD", txtPass.Text);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                bind_data();
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("Update dbsve Set NAME=@NAME , PHONE=@PHONE , PASSWORD=@PASSWORD  where ID=@ID", conn);
+            cmd.Parameters.AddWithValue("ID", txtId.Text);
+            cmd.Parameters.AddWithValue("NAME", txtNm.Text);
+            cmd.Parameters.AddWithValue("PHONE", txtPho.Text);
+            cmd.Parameters.AddWithValue("PASSWORD", txtPass.Text);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            bind_data();
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index;
+            index = e.RowIndex;
+            DataGridViewRow selectedRow = dataGridView1.Rows[index];
+            txtId.Text = selectedRow.Cells[0].Value.ToString();
+            txtNm.Text = selectedRow.Cells[1].Value.ToString();
+            txtPho.Text = selectedRow.Cells[2].Value.ToString();
+            txtPass.Text = selectedRow.Cells[3].Value.ToString();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("delete from dbsve where ID= @ID ", conn);
+            cmd.Parameters.AddWithValue("ID",txtId.Text);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            bind_data();
+
+        }
+
+        private void grid(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("SELECT ID , NAME  ,PHONE, PASSWORD FROM dbsve WHERE ID LIKE @ID+'%' OR NAME LIKE @NAME+'%'", conn);
+            cmd.Parameters.AddWithValue("ID", txtSearch.Text);
+            cmd.Parameters.AddWithValue("NAME",txtSearch.Text);
+            SqlDataAdapter sda = new SqlDataAdapter();
+            sda.SelectCommand = cmd;
+            DataTable dt = new DataTable();
+            dt.Clear();
+            sda.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("tohome", 12, FontStyle.Bold);
+            dataGridView1.DefaultCellStyle.Font = new Font("arial", 12, FontStyle.Bold);
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            printPreviewDialog1.Document = printDocument1;
+            printPreviewDialog1.PrintPreviewControl.Zoom = 1;
+            printPreviewDialog1.ShowDialog();
+
+
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Bitmap bm = new Bitmap(dataGridView1.Width, dataGridView1.Height);
+            dataGridView1.DrawToBitmap(bm, new Rectangle(0, 0, dataGridView1.Width, dataGridView1.Height));
+            e.Graphics.DrawImage(bm, 120, 20);
+
+        }
+    }
+}
